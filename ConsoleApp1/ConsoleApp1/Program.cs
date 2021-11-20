35e37a61922ff48678ca5a43c467ae290f42b23b
vish <vishgirase95@gmail.com>
2021-11-20 22:44:51 +0530

    [vishal] added contacts

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,15 +5,21 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public class contact
+        {
+            string FirstName;
+            string LasttName;
+            int Phone;
+            int Pincode;
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Adress Book");
 
-            List<string> contact = new List<string>();
-            contact.Add("vishal");
-            contact.Add("vishal");
-            contact.Add("vishal");
-            contact.Add("vishal");
+            List<string> AdressBook = new List<string>();
+      
 
         }
     }
